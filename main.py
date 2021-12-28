cd3f7aa8b1654f118c2d8e6e74774490e76e9a78
aleruijur <aleruijur@alum.us.es>
2021-12-28 23:30:01 +0100

    Añadido el html como query string

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pdfkit 
 import pathlib
 import platform
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Template#, Environment, FileSystemLoader
 
 
 class Diploma:
@@ -14,8 +14,14 @@ class Diploma:
     self.date = _date
   
   def generate(self):
-    env = Environment(loader=FileSystemLoader("./plantillas"))
-    template=env.get_template(self.diploma_a_generar)
+    # ----------- Si se quiere usar el directorio de plantillas de la api ------------
+    #env = Environment(loader=FileSystemLoader("./plantillas"))
+    #template=env.get_template(self.diploma_a_generar)
+
+    # ----------- Si se quiere dar la plantilla como parámetro de la petición ------------
+    template = Template(self.diploma_a_generar)
+    # ----------- 
+
     usuario ={
         
     'name' : self.nombre_alumno,
@@ -43,10 +49,61 @@ class Diploma:
     #   path_wkhtmltopdf = str(path) + "\wkhtmltox_0.12.6-1.focal_amd64.deb"
       
     #config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-    print("Generando diploma de la plantilla '" +self.diploma_a_generar+"'")
+    print("Generando diploma de la plantilla ...")
     pdfkit.from_string(html,self.nombre_diploma,options=options)
     print("Diploma generado correctamente")
     return True
 
-diploma = Diploma("diploma_ctf.html","prueba 1.pdf","antonio","4","2","hoy")
+#----Html de la plantilla a usar----
+html = """<!DOCTYPE html>
+<html>
+
+<head>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
+    html,body{height: 70%;}
+    body{
+        background-image: url("https://www.detoxpobyt.sk/wp-content/uploads/2017/07/response.jpg");
+        /* https://www.insperity.com/wp-content/uploads/organization_structure1200x600.png */
+        background-size: cover;
+        background-repeat: no-repeat;
+        font-family: 'Poppins';
+        font-weight: 300;
+        height: 100%;
+        text-align: center;
+        color: #2a7a31;
+        text-shadow: 3px 3px 4px #000000;
+    }
+    .bold{
+        font-weight: 300;
+    }
+</style>
+</head>
+
+<body>
+    <div style= "padding-top: 80px;">
+        <div>
+            <h1 class="bold">DIPLOMA AL MEJOR COMITÉ</h1>
+            <p style="margin-bottom: 0px;margin-top: 25px;" class="light">Da la enhorabuena a:</p>
+            <p style="font-size: 2.4em;margin:0px" class="bold">{{name}}</p>
+            <br>
+            <br>
+
+            <p style="margin:0px;">Por formar parte de:</p>
+            <h2 style="max-width: 600px;margin:25px auto;font-size: 3em;" class="bold">{{course}}</h2>
+
+            <br>
+            <br>
+            <br>
+            <br>
+
+            <p style="font-size: 2.4em;margin:0px;">Nombrado el mejor comité de la edición:</p>
+            <p style="font-size: 1.5em;" class="bold">{{date}}</p>
+
+
+        </div>
+    </div>
+</body>
+</html>"""
+diploma = Diploma(html,"prueba 1.pdf","Alejandro Ruiz","Comité de Programa","2","XI Edición Innosoft 2021")
 diploma.generate()

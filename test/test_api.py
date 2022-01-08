d28c7f5700aba09329624c691a08ee529438e5bd
ferclabar <fernandoclarosb@gmail.com>
2022-01-08 20:24:16 +0100

    pruebas sobre api casos positivo y negativo #25

---
+++ test/test_api.py
@@ -0,0 +1,30 @@
+
+
+
+from selenium import webdriver
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+    
+    
+        
+    
+
+def test_api():
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
+    response= driver.get("https://google.com")
+    print(response)
+    driver.quit()
+
+
+
+
+
+        

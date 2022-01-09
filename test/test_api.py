d508807f86d23751cce7c4c789f7cd1a91efc22e
ferclabar <fernandoclarosb@gmail.com>
2022-01-09 15:46:04 +0100

    Pruebas sobre API #25

--- test/test_api.py
+++ test/test_api.py
@@ -1,30 +1,41 @@
 
-
-
-from selenium import webdriver
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+import unittest
 
+class SeleniumTest(unittest.TestCase):
+    def test_generate(self):
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        driver = webdriver.Chrome(options=options)
 
+        driver.get("https://generador-diplomas-innosoft.herokuapp.com/diploma?diplomaGenerar=wasd&nombreDiploma=prueba1&name=Alejandro Ruiz&course=Comité de Programa&score=2&date=XI Edición Innosoft 2021&mailto=fernandoclarosb@gmail.com")
 
+        self.assertEqual('{"status":"Diploma generado correctamente.Mail enviado a 'fernandoclarosb@gmail.com'"}', driver.find_element_by_tag_name('pre').text)
+        driver.quit()
     
-    
-        
-    
 
-def test_api():
-    options = webdriver.ChromeOptions()
-    options.headless = True
-    driver = webdriver.Chrome(options=options)
-    response= driver.get("https://google.com")
-    print(response)
-    driver.quit()
+    def test_generate_error_mail(self):
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        driver = webdriver.Chrome(options=options)
 
+        driver.get("https://generador-diplomas-innosoft.herokuapp.com/diploma?diplomaGenerar=wasd&nombreDiploma=prueba1&name=Alejandro Ruiz&course=Comité de Programa&score=2&date=XI Edición Innosoft 2021&mailto=kikii")
 
+        self.assertEqual("Internal Server Error", driver.find_element_by_tag_name('h1').text)
+        driver.quit()
 
+    def test_generate_error_mail(self):
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        driver = webdriver.Chrome(options=options)
 
+        driver.get("https://generador-diplomas-innosoft.herokuapp.com/diploma?diplomaGenerar=wasd&nombreDiploma=prueba1&name=Alejandro Ruiz&course=Comité de Programa&score=2&date=XI Edición Innosoft 2021&mailto=kikii")
 
-        
+        self.assertEqual("Internal Server Error", driver.find_element_by_tag_name('h1').text)
+        driver.quit()
+
+if name == "main":
+    unittest.main()

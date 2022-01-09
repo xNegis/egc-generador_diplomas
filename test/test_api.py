a8f5638f69a463c0ac40decf3ac14c6daf2edc40
xNegis <antammar4@alum.us.es>
2022-01-09 16:47:22 +0100

    Integracion Continua #27

--- test/test_api.py
+++ test/test_api.py
@@ -16,9 +16,9 @@ class SeleniumTest(unittest.TestCase):
 
     def test_generate(self):
         
-        DRIVER.get(URL_APP+"/diploma?diplomaGenerar=wasd&nombreDiploma=prueba&name=Alejandro&course=Comité%20de%20Programa&score=2&date=2020-02-02&mailto=fernandoclarosb@gmail.com")
+        DRIVER.get(URL_APP+"/diploma?diplomaGenerar=wasd&nombreDiploma=prueba&name=Alejandro&course=Comité%20de%20Programa&score=2&date=2020-02-02&mailto=generadordiplomasinnosoft@gmail.com")
 
-        self.assertEqual('{"status":"Diploma generado correctamente.Mail enviado a \'fernandoclarosb@gmail.com\'"}', DRIVER.find_element_by_tag_name('pre').text)
+        self.assertEqual('{"status":"Diploma generado correctamente.Mail enviado a \'generadordiplomasinnosoft@gmail.com\'"}', DRIVER.find_element_by_tag_name('pre').text)
 
 
     def test_generate_error_mail(self):
@@ -36,19 +36,19 @@ class SeleniumTest(unittest.TestCase):
 
     def test_generate_error_nombre(self):
 
-        DRIVER.get(URL_APP+"/diploma?diplomaGenerar=wasd&nombreDiploma=prueba1&name=Alejandro%20Ruiz&course=Comité%20de%20Programa&score=2&date=2020-02-02&mailto=xnegis@gmail.com")
+        DRIVER.get(URL_APP+"/diploma?diplomaGenerar=wasd&nombreDiploma=prueba1&name=Alejandro%20Ruiz&course=Comité%20de%20Programa&score=2&date=2020-02-02&mailto=generadordiplomasinnosoft@gmail.com")
 
         self.assertTrue("Nombre: " in DRIVER.find_element_by_tag_name('pre').text)
 
     def test_generate_error_nombre_vacio(self):
 
-        DRIVER.get(URL_APP+"/diploma?diplomaGenerar=wasd&nombreDiploma=prueba1&name=&course=Comité%20de%20Programa&score=2&date=2020-02-02&mailto=xnegis@gmail.com")
+        DRIVER.get(URL_APP+"/diploma?diplomaGenerar=wasd&nombreDiploma=prueba1&name=&course=Comité%20de%20Programa&score=2&date=2020-02-02&mailto=generadordiplomasinnosoft@gmail.com")
 
         self.assertTrue("Nombre: " in DRIVER.find_element_by_tag_name('pre').text)
 
     def test_generate_error_diploma_generar_vacio(self):
 
-        DRIVER.get(URL_APP+"/diploma?diplomaGenerar=&nombreDiploma=holaprueba1&name=Alejandro&course=Comité%20de%20Programa&score=2&date=2020-02-02&mailto=xnegis@gmail.com")
+        DRIVER.get(URL_APP+"/diploma?diplomaGenerar=&nombreDiploma=holaprueba1&name=Alejandro&course=Comité%20de%20Programa&score=2&date=2020-02-02&mailto=generadordiplomasinnosoft@gmail.com")
 
         self.assertTrue("Diploma a Generar: " in DRIVER.find_element_by_tag_name('pre').text)
 

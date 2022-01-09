f73f0a87d0c6a9ee8341295b81dc83f54aa8d618
xNegis <antammar4@alum.us.es>
2022-01-09 16:46:43 +0100

    Integracion Continua #27

--- test/test_api.py
+++ test/test_api.py
@@ -3,39 +3,57 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+import pathlib
 import unittest
 
+PATH = str(pathlib.Path().resolve())
+OPTIONS = webdriver.ChromeOptions()
+OPTIONS.headless = True
+DRIVER = webdriver.Chrome(PATH+"/test/chromedriver.exe",options=OPTIONS)
+URL_APP ="https://generador-diplomas-innosoft-2.herokuapp.com"
+
 class SeleniumTest(unittest.TestCase):
+
     def test_generate(self):
-        options = webdriver.ChromeOptions()
-        options.headless = True
-        driver = webdriver.Chrome(options=options)
+        
+        DRIVER.get(URL_APP+"/diploma?diplomaGenerar=wasd&nombreDiploma=prueba&name=Alejandro&course=Comité%20de%20Programa&score=2&date=2020-02-02&mailto=fernandoclarosb@gmail.com")
 
-        driver.get("https://generador-diplomas-innosoft.herokuapp.com/diploma?diplomaGenerar=wasd&nombreDiploma=prueba1&name=Alejandro Ruiz&course=Comité de Programa&score=2&date=XI Edición Innosoft 2021&mailto=fernandoclarosb@gmail.com")
+        self.assertEqual('{"status":"Diploma generado correctamente.Mail enviado a \'fernandoclarosb@gmail.com\'"}', DRIVER.find_element_by_tag_name('pre').text)
 
-        self.assertEqual('{"status":"Diploma generado correctamente.Mail enviado a 'fernandoclarosb@gmail.com'"}', driver.find_element_by_tag_name('pre').text)
-        driver.quit()
-    
 
     def test_generate_error_mail(self):
-        options = webdriver.ChromeOptions()
-        options.headless = True
-        driver = webdriver.Chrome(options=options)
 
-        driver.get("https://generador-diplomas-innosoft.herokuapp.com/diploma?diplomaGenerar=wasd&nombreDiploma=prueba1&name=Alejandro Ruiz&course=Comité de Programa&score=2&date=XI Edición Innosoft 2021&mailto=kikii")
+        DRIVER.get(URL_APP+"/diploma?diplomaGenerar=wasd&nombreDiploma=prueba1&name=Alejandro&course=Comité%20de%20Programa&score=2&date=2020-02-02&mailto=kikii")
 
-        self.assertEqual("Internal Server Error", driver.find_element_by_tag_name('h1').text)
-        driver.quit()
+        self.assertTrue("Correo: " in DRIVER.find_element_by_tag_name('pre').text)
 
-    def test_generate_error_mail(self):
-        options = webdriver.ChromeOptions()
-        options.headless = True
-        driver = webdriver.Chrome(options=options)
+    def test_generate_error_mail_vacio(self):
 
-        driver.get("https://generador-diplomas-innosoft.herokuapp.com/diploma?diplomaGenerar=wasd&nombreDiploma=prueba1&name=Alejandro Ruiz&course=Comité de Programa&score=2&date=XI Edición Innosoft 2021&mailto=kikii")
+        DRIVER.get(URL_APP+"/diploma?diplomaGenerar=wasd&nombreDiploma=prueba1&name=Alejandro&course=Comité%20de%20Programa&score=2&date=2020-02-02&mailto=")
 
-        self.assertEqual("Internal Server Error", driver.find_element_by_tag_name('h1').text)
-        driver.quit()
+        self.assertTrue("Correo: " in DRIVER.find_element_by_tag_name('pre').text)
 
-if name == "main":
+
+    def test_generate_error_nombre(self):
+
+        DRIVER.get(URL_APP+"/diploma?diplomaGenerar=wasd&nombreDiploma=prueba1&name=Alejandro%20Ruiz&course=Comité%20de%20Programa&score=2&date=2020-02-02&mailto=xnegis@gmail.com")
+
+        self.assertTrue("Nombre: " in DRIVER.find_element_by_tag_name('pre').text)
+
+    def test_generate_error_nombre_vacio(self):
+
+        DRIVER.get(URL_APP+"/diploma?diplomaGenerar=wasd&nombreDiploma=prueba1&name=&course=Comité%20de%20Programa&score=2&date=2020-02-02&mailto=xnegis@gmail.com")
+
+        self.assertTrue("Nombre: " in DRIVER.find_element_by_tag_name('pre').text)
+
+    def test_generate_error_diploma_generar_vacio(self):
+
+        DRIVER.get(URL_APP+"/diploma?diplomaGenerar=&nombreDiploma=holaprueba1&name=Alejandro&course=Comité%20de%20Programa&score=2&date=2020-02-02&mailto=xnegis@gmail.com")
+
+        self.assertTrue("Diploma a Generar: " in DRIVER.find_element_by_tag_name('pre').text)
+
+
+
+
+if __name__ == "main":
     unittest.main()
